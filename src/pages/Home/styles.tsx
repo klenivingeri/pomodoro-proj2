a0268df5d7b0c8d9ff0d4af60a0178ec591f9fb6
klenivingeri <libertti@live.com>
2023-12-12 20:19:24 -0300

    chore: Páginas e rotas - Aprimorando os inputs

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -49,6 +49,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex:1;
+
+  &::-webkit-calendar-picker-indicator { /*remover a seta da lista*/
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
